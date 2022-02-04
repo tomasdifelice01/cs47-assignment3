5cb20fa4096471f28b40337badb5f40e0fc2711b
tomasdifelice01 <difelice@stanford.edu>
2022-02-03 16:02:31 -0800

    adding files

---
+++ App.js
@@ -0,0 +1,62 @@
+import { StyleSheet, Text, SafeAreaView } from "react-native";
+import { useState, useEffect } from "react";
+import { ResponseType, useAuthRequest } from "expo-auth-session";
+import { myTopTracks, albumTracks } from "./utils/apiOptions";
+import { REDIRECT_URI, SCOPES, CLIENT_ID, ALBUM_ID } from "./utils/constants";
+import Colors from "./Themes/colors"
+
+// Endpoints for authorizing with Spotify
+const discovery = {
+  authorizationEndpoint: "https://accounts.spotify.com/authorize",
+  tokenEndpoint: "https://accounts.spotify.com/api/token"
+};
+
+export default function App() {
+  const [token, setToken] = useState("");
+  const [tracks, setTracks] = useState([]);
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      responseType: ResponseType.Token,
+      clientId: CLIENT_ID,
+      scopes: SCOPES,
+      // In order to follow the "Authorization Code Flow" to fetch token after authorizationEndpoint
+      // this must be set to false
+      usePKCE: false,
+      redirectUri: REDIRECT_URI
+    },
+    discovery
+  );
+
+  useEffect(() => {
+    if (response?.type === "success") {
+      const { access_token } = response.params;
+      setToken(access_token);
+    }
+  }, [response]);
+
+  useEffect(() => {
+    if (token) {
+      // TODO: Select which option you want: Top Tracks or Album Tracks
+
+      // Comment out the one you are not using
+      // myTopTracks(setTracks, token);
+      albumTracks(ALBUM_ID, setTracks, token);
+    }
+  }, [token]);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      {/* TODO */}
+      <Text style={{ color: "white" }}>Welcome to Assignment 3 - Spotify</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.background,
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1
+  }
+});
---
+++ utils/constants.js
@@ -0,0 +1,13 @@
+export const CLIENT_ID = "TODO: INSERT YOUR CLIENT ID HERE";
+export const SCOPES = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+  "streaming",
+  "user-read-email",
+  "user-read-private"
+];
+export const REDIRECT_URI = "TODO: INSERT YOUR REDIRECT_URI HERE";
+export const ALBUM_ID = "TODO: INSERT YOUR ALBUM ID HERE IF YOU ARE USING ONE";
